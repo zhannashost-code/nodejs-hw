3ca659bc7ffe0657f61f451247685545a4f4cfc6
zhannashost-code <zhannashost@gmail.com>
2026-08-14 17:41:47 +0300

    add hw-node.js-01

---
+++ src/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
+
+dotenv.config();
+
+const app = express(); // app - web-server
+
+app.use(express.json());
+
+const logger = pinoHttp({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss',
+      ignore: 'pid,hostname',
+      messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+      hideObject: true,
+    },
+  },
+});
+app.use(logger);
+
+app.use(cors());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+  });
+});
+
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, () => console.log(`Server running on ${port} port`));
